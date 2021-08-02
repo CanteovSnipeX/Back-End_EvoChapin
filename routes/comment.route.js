2be0e74952ab2705ade83bb527e36af962d7357a
CanteovSnipeX <84818199+CanteovSnipeX@users.noreply.github.com>
2021-08-02 08:58:53 -0600

    ...

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -4,7 +4,7 @@ var api = express.Router();
 var mdAuth = require("../middlewares/authenticated");
 
 
-api.post('/setComment',mdAuth.ensureUser,commentController.setComment);
+api.post('/setComment',commentController.setComment);
 api.put('/removeComment/:id',[mdAuth.ensureUser,mdAuth.ensureAdmin],commentController.removeComment);
 api.get('/getComment',[mdAuth.ensureUser,mdAuth.ensureAdmin],commentController.getComment);
 
